6bd80a8ae89c95bbfbc8d3f4b641048a3d19b265
Breno Bonani <BrenoBonani@MacBook-Pro.local>
2022-07-01 09:21:31 -0300

    Updates

---
+++ precache-manifest.7089cb4f1ed2c0fa69f8073bfed0344d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8016ddc3bd349569ddd3da6c96e1b2a8",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/react-todolist/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "870127e59e371077e12a",
+    "url": "/react-todolist/static/js/main.82f0e841.chunk.js"
+  },
+  {
+    "revision": "03d33efdc612aefe6269",
+    "url": "/react-todolist/static/js/runtime-main.dce78e83.js"
+  }
+]);
